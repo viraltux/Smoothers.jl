a6a41941a296648f84ecd5a1fbdd4be9fa8b4167
Fran Urbano <viraltux@gmail.com>
2021-09-01 11:48:30 +0200

    update test/sma

--- test/sma.jl
+++ test/sma.jl
@@ -11,9 +11,9 @@ using Smoothers
 
     x = sma(1:5,3,true)
     @test length(x) == 5
-    @test x == [1.0, 2.0, 3.0, 4.0, 5.0]
+    @test ismissing.(x) == Bool[1,0,0,0,1]
+    @test x[2:4] == [2.0, 3.0, 4.0]
     
-
     x = sma(1:5,3,false)
     @test length(x) == 3
     @test x == [2.0, 3.0, 4.0]
