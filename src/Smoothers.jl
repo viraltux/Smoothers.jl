5d47b7d9e401d7c590c1bc5f1504e212637e8d86
Fran Urbano <viraltux@gmail.com>
2021-09-02 19:00:14 +0200

    update Smoothers docstring

--- src/Smoothers.jl
+++ src/Smoothers.jl
@@ -12,4 +12,16 @@ include("loess.jl")
 include("sma.jl")
 include("stl.jl")
 
+"""
+Collection of smoothing heuristics, models and smoothing related applications. The current available smoothers and applications are:
+
+    `filter`: Linear Time-invariant Difference Equation Filter (Matlab/Octave)
+    `hma`:    Henderson Moving Average Filter
+    `loess`:  Locally Estimated Scatterplot Smoothing
+    `sma`:    Simple Moving Average
+    `stl`:    Seasonal and Trend decomposition based on Loess
+
+"""
+Smoothers
+
 end
