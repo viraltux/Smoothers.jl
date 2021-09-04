28a779948e4368f7e891a23c8215146df70c4d84
Fran Urbano <viraltux@gmail.com>
2021-09-04 15:56:15 +0200

    log large products and increase performance

--- src/hma.jl
+++ src/hma.jl
@@ -25,7 +25,7 @@
 """
 Package: Forecast
 
-    hma(s, n)
+    hma(x, n)
 
 Applies the Henderson moving average filter to dataset `s` with `n`-term.
 
@@ -46,48 +46,50 @@ smoothed value for a given point in the time series is calculated from an equal 
 on either side of the data point." - Australian Bureau of Statistics (www.abs.gov.au)
 
 # Arguments
-- `s`: Observations' support.
-- `n`: Observation values. Tipically 13 or 7 for quarterly data, larger values may need a BigInt type.
+- `x`: Observations' support.
+- `n`: Observation values, tipically 13 or 7 for quarterly data.
 
 # Returns
 An array of Henderson filter smoothed values provided in `s`.
 
 # Examples
 ```julia-repl
-julia> hma(rand(1000), BigInt(303)))
-1000-element Vector{BigFloat}:
+julia> hma(rand(1000), 303)
+1000-element Vector{Float64}}:
 [...]
 ```
 """
-@inline function hma(s::AbstractVector{<:T}, n::Integer) where T<:Real
+@inline function hma(x::AbstractVector{<:T}, n::Integer) where T<:Real
 
     @assert isodd(n) "n must be odd"
     @assert n >= 5 "n must be greater or equal 5"
-    @assert length(s) >= n "length(s) must be greater or equal to n"
+    @assert (lx = length(x)) >= n "length(s) must be greater or equal to n"
 
-    # Promote to equivalent in size T Integer type andd Float type
-    n = typeof(Integer(T(1.0)))(n)
-    s,_ = Base.promote(collect(s),[Base.promote_op(/,T,T)(1.0)])
-    
-    w = hmaSymmetricWeights(n)
+    # Promotion to Float type
+    P = Base.promote_op(/,T,T)
+    x,_ = Base.promote(collect(x),[P(1.0)])
+
+    w = P.(hmaSymmetricWeights(n))
     
     m = (n-1) ÷ 2
-    ls = length(s)
+
+    ic = n < 13 ? P(1.0) : (13 <= n < 15 ? P(3.5) : P(4.5))
+    b2s2 = P(4.0)/P(pi)/ic^2
 
     function hmai(i)
         if i - 1 < m
-            u = hmaAsymmetricWeights(m + i, w)[end:-1:1]
-            sum(s[1:i + m] .* u)
-        elseif i - 1 + m >= ls
-            u = hmaAsymmetricWeights(m + ls - i + 1, w)
-            sum(s[i-m:ls] .* u)
+            u = hmaAsymmetricWeights(m + i, w, b2s2)[end:-1:1]
+            sum(x[1:i + m] .* u)
+        elseif i - 1 + m >= lx
+            u = hmaAsymmetricWeights(m + lx - i + 1, w, b2s2)
+            sum(x[i-m:lx] .* u)
         else
-            sum(s[i-m:i+m] .* w)
+            sum(x[i-m:i+m] .* w)
         end
     end
     
-    hmav = similar(s)
-    for i in 1:ls
+    hmav = similar(x)
+    for i in 1:lx
         @inbounds hmav[i] = hmai(i)
     end
     hmav
@@ -117,15 +119,17 @@ Caluclate the hma symmetric weights for 'n'
     m1 = (m+1)^2
     m2 = (m+2)^2
     m3 = (m+3)^2
-    d = 315/(8*(m+2)*(m2-1)*(4*m2-1)*(4*m2-9)*(4*m2-25))
-
+    logd = log(m+2)+log(m2-1)+log(4*m2-1)+log(4*m2-9)+log(4*m2-25)
+    d = 315/(8*exp(logd))
+    
     P = Base.promote_op(/,T,T)
     w = Vector{P}(undef,m+2)
     m,m1,m2,m3,d = promote(m,m1,m2,m3,d,P(1.0))
-
-    for (i,x) in enumerate(0:m+1)
-        @inbounds w[i] = (m1-x^2)*(m2-x^2)*(m3-x^2)*(P(3.0)*m2-P(11.0)*x^2-P(16.0))*d
+    for (i,v) in enumerate(0:m+1)
+         @inbounds w[i] = real(exp(log(m1-v^2)+log(m2-v^2)+log(m3-v^2)+
+                               log(complex(P(3.0)*m2-P(11.0)*v^2-P(16.0)))+log(d)))
     end
+
     u = vcat(w[end-1:-1:1],w[2:end-1])
 
     return mod(n, 2) != 0 ? u : vcat(u, missing)
@@ -151,23 +155,20 @@ Vector of asymmetrical hma weights
 
 - Mike Doherty (2001), "The Surrogate Henderson Filters in X-11",Aust, NZ J of Stat. 43(4), 2001, 385–392
 """
-@inline function hmaAsymmetricWeights(m::Integer, w::AbstractVector{<:T}) where T<:Real
+@inline function hmaAsymmetricWeights(m::Integer, w::AbstractVector{<:T}, b2s2::T) where T<:Real
 
     n = length(w)
-
-    @assert m <= n "The m argument must be less than w"
-    @assert m >= (n-1)÷2 "The m argument must be greater than (n-1)/2"
+    # @assert m <= n "The m argument must be less than w"
+    # @assert m >= (n-1)÷2 "The m argument must be greater than (n-1)/2"
 
     sumResidual = sum(w[range(m + 1, n, step = 1)]) / T(m)
     
     sumEnd = T(0.0)
+    m12 = (m+T(1.0))/T(2.0)
     for i in m+1:n
-        @inbounds sumEnd += (i-(m+T(1.0))/T(2.0))*w[i]
+        @inbounds sumEnd += (i-m12)*w[i]
     end
     
-    ic = n < 13 ? T(1.0) : (13 <= n < 15 ? T(3.5) : T(4.5))
-    
-    b2s2 = T(4.0)/T(pi)/ic^2
     denominator = T(1.0) + ((m*(m-T(1.0))*(m+T(1.0)) / T(12.0) ) * b2s2)
 
     aw = Vector{T}(undef,m)
